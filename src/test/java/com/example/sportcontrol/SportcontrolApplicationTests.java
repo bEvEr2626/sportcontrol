40a57abd99ab4430407f143f3eb3cae9660b9a3c
bEvEr2626 <silabobrow@gmail.com>
2026-02-21 18:40:40 +0300

    feat: initial sportcontrol  project

---
+++ src/test/java/com/example/sportcontrol/SportcontrolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sportcontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportcontrolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
