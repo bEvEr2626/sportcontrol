7289796d48bd90e690d554237369f7ced7cbfa6f
bEvEr2626 <silabobrow@gmail.com>
2026-04-11 23:11:44 +0300

    tests

--- src/test/java/com/example/sportcontrol/SportcontrolApplicationTests.java
+++ src/test/java/com/example/sportcontrol/SportcontrolApplicationTests.java
@@ -4,7 +4,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+    "spring.datasource.url=jdbc:h2:mem:testdb",
+    "spring.datasource.driver-class-name=org.h2.Driver",
+    "spring.datasource.username=sa",
+    "spring.datasource.password="
+})
 @ActiveProfiles("test")
 class SportcontrolApplicationTests {
 
